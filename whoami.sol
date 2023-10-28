9c8f081cef321bf0a26b324b0224ddf682388da7
MostPow3rful <itskianshahi@gmail.com>
2023-10-28 17:59:39 +0330

    Created [showAddress] Function

--- whoami.sol
+++ whoami.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract WhoAmI {}
+contract WhoAmI {
+    function showAddress() public view returns(address yourAddress) {
+        return msg.sender;
+    }
+}
