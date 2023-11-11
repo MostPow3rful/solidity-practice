960d98971281f9ac8961dd01ebf5d50617428591
MostPow3rful <itskianshahi@gmail.com>
2023-11-11 20:13:48 +0330

    New Events Added due better Log

--- error.sol
+++ error.sol
@@ -2,5 +2,10 @@
 pragma solidity ^0.8.20;
 
 contract Error {
+    // [ Errors ]
     error accountStatus(bool _haveAccount, string _message);
+    
+    // [ Events ]
+    event ErrorTryingToCreateAccount(address owner);
+    event ErrorTryingToResetAccount(address owner);
 }
